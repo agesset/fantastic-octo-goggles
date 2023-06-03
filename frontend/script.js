1ac6f0400eafd0de3da41801d2ed92d86ccb4a26
Zed <11563693+zedauni@users.noreply.github.com>
2023-06-03 04:45:41 +0100

    👌 Move API call and DOM manipulation to function
    to make it reusable for other API calls

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,33 @@
 const api_url = 'https://lamenarrowformats--prunelmel.repl.co'
 const selectDep = document.querySelector("#dep")
-url = api_url + '/dep'
 
 //Dep
-axios
-    .get(url)
-    .then(function (response) {
-        // handle success
-        if (response.status > 200 || response.status < 300) {
 
-            let options = ''
-            for (const item in response.data) {
-                options += '<option value="' + response.data[item] + '">' + response.data[item] + '</option>'
+
+/**
+ * Do API async call and dom manipulation
+ * @param {string} resource Resource to fetch from API
+*/
+function doApiLogic(resource) {
+    url = api_url + '/' + resource
+    axios
+        .get(url)
+        .then(function (response) {
+            // handle success
+            if (response.status > 200 || response.status < 300) {
+
+                let options = ''
+                for (const item in response.data) {
+                    options += '<option value="' + response.data[item] + '">' + response.data[item] + '</option>'
+                }
+                document.querySelector("#" + resource).innerHTML = options
+            } else {
+                alert("Something went wrong")
             }
-            document.querySelector("#dep").innerHTML = options
-        } else {
-            alert("Something went wrong")
-        }
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error)
-        alert(error.toJSON())
-    }).finally(function () { })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error)
+            alert(error.toJSON())
+        }).finally(function () { })
+}
